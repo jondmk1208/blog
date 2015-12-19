4f828072e85df5997a577c1ed5b06e5105278121
jondmk <jondmk1208@gmail.com>
2015-12-19 03:17:10 -0800

    Sendgrid

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,12 +2,12 @@ class Contact < MailForm::Base
 	attribute :name,     :validate => true
 	attribute :email,    :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
 	attribute :message,  :validate => true
-	attribute :nickname, :captcha => true
+	attribute :nickname, :captcha  => true
 
 	def headers 
 		{
 		:subject => "Contact Form",
-		:to => "jondmk@jondmk.com",
+		:to => "jondmk1208@gmail.com",
 		:from => %("#{name}" <#{email}>)
 		}
 	end
